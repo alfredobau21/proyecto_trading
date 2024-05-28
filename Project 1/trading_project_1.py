94de4a5dedb17c9cd616558d9e8de84a2edebb01
Alfredo Bautista <carlos.bautista@iteso.mx>
2024-05-28 15:01:05 -0600

    PDF

--- Project 1/trading_project_1.py
+++ Project 1/trading_project_1.py
@@ -14,3 +14,20 @@ warnings.filterwarnings('ignore')
 import numpy as np
 from scipy.optimize import minimize
 from scipy.stats import lognorm
+
+
+# Plot the Price Distribution
+lambda_param = 50
+k_param = 10
+
+x = np.linspace(0, 100, 1000)
+pdf = (k_param / lambda_param) * (x / lambda_param)**(k_param - 1) * np.exp(-(x / lambda_param)**(k_param))
+
+# Plot the PDF
+plt.figure(figsize=(8, 6))
+plt.plot(x, pdf, 'r-', lw=2, label='Weibull PDF')
+plt.title('Weibull Probability Density Function')
+plt.xlabel('Price')
+plt.ylabel('Probability Density')
+plt.legend(loc = 'best')
+plt.show()
