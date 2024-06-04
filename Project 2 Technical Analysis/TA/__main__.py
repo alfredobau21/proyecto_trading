89a6c1dafe885fadc150a33deac60d99d5603be2
a ver qp <manuel.reyna@iteso.mx>
2024-06-04 16:22:25 -0600

    empezando de 0

--- Project 2 Technical Analysis/TA/__main__.py
+++ Project 2 Technical Analysis/TA/__main__.py
@@ -2,22 +2,3 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import ta
 import optuna
-
-class TechnicalData:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.data = self.load_data()
-        self.technical_data = self.extract_technical_data()
-
-    def load_data(self):
-        return pd.read_csv(self.file_path).dropna()
-
-    def extract_technical_data(self):
-        technical_data = pd.DataFrame()
-        technical_data["Close"] = self.data["Close"]
-        return technical_data
-
-technical_data_A1 = TechnicalData("data/aapl_project_1m_train.csv")
-technical_data_A5 = TechnicalData("data/aapl_project_train.csv")
-technical_data_B1 = TechnicalData("data/btc_project_1m_train.csv")
-technical_data_B5 = TechnicalData("data/btc_project_train.csv")
